f4395f1d5f98555af54019cc19d771283d6e8460
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-03-12 10:37:27 -0700

    updated models and readme. added config file for auth

--- server/config/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    secret: 'bi#loj^-4ng2g3@st14-01~s!2rin3gasdfk)*kjni13j90pui(&j2afd!24pang9',
-    db_connect: 'mongodb://auth_demo:auth_demo@ds241578.mlab.com:41578/nodeauthexample'
-}
--- server/models/students.js
+++ server/models/students.js
@@ -3,34 +3,10 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt-nodejs');
 
 const studentSchema = new Schema({
-    email: String,
-    password: String,
-    givenName: String
+    givenName: String,
+    familyName: String,
+    courses: Object
 })
-
-//callback function definition using the pre method
-studentSchema.pre('save', function (next) {
-    const user = this;
-
-    bcrypt.genSalt(10, (err, salt) => {
-        if (err) return next(err);
-        bcrypt.hash(user.password, salt, null, (err, hash) => {
-            if (err) return next(err);
-
-            user.password = hash;
-
-            next();
-        })
-    })
-})
-
-studentSchema.methods.comparePasswords = function (candidate, callback) {
-    bcrypt.compare(candidate, this.password, (err, isMatch) => {
-        if (err) return callback(err);
-
-        callback(null, isMatch)
-    })
-}
 
 //create model for 'student' collection
 const ModelClass = mongoose.model('student', studentSchema)
